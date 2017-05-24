00d5141cc93fffe5243ef69ad433caedb03cb4e8
Yoan Blanc <yoan@dosimple.ch>
2017-05-24 18:34:28 +0200

    Requires 3.6.
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- travisbot/__init__.py
+++ travisbot/__init__.py
@@ -1,6 +1,13 @@
 """Travis bot package."""
 
+import sys
+
 from .api import api  # noqa
 from .bot import bot  # noqa
 from .conf import URL, HOST, PORT, TRAVIS_CONFIG_URL  # noqa
 from .web import make_app  # noqa
+
+
+if sys.version < (3, 6):
+    raise ImportError("travisbot requires Python 3.6+ because f-str/asyncio. "
+                      "<3")
