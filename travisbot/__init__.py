5ad1b48ea5b8548d1e275c291d08bb0b1e64b397
Yoan Blanc <yoan@dosimple.ch>
2017-05-28 16:32:38 +0200

    Fix version_info test... (shame)
    Signed-off-by: Yoan Blanc <yoan@dosimple.ch>

--- travisbot/__init__.py
+++ travisbot/__init__.py
@@ -8,6 +8,6 @@ from .conf import URL, HOST, PORT, TRAVIS_CONFIG_URL  # noqa
 from .web import make_app  # noqa
 
 
-if sys.version < (3, 6):
+if sys.version_info < (3, 6):
     raise ImportError("travisbot requires Python 3.6+ because f-str/asyncio. "
                       "<3")
